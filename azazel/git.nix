2cd129a8205ea29c8f4402c1e449e6b497558431
Alberto Berti <alberto@metapensiero.it>
2021-08-20 15:24:35 +0200

    Add git config

---
+++ azazel/git.nix
@@ -0,0 +1,22 @@
+{ config, lib, pkgs, ... }: {
+  programs.git = {
+    enable = true;
+    aliases = {
+      ci = "commit";
+      co = "checkout";
+      st = "status";
+      sum = "log --graph --decorate --oneline --color --all";
+    };
+    delta.enable = true;
+    extraConfig = {
+      color.ui = "auto";
+      pull.rebase = "false";
+    };
+    includes = [{
+      condition = "gitdir:~/wip/endian";
+      contents = { user.email = "a.berti@endian.com"; };
+    }];
+    userName = "Alberto Berti";
+    userEmail = "alberto@metapensiero.it";
+  };
+}
