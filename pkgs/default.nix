f0acf78b14f885f9ed8923c76fa0432bf63b502f
Alberto Berti <alberto@metapensiero.it>
2021-08-22 14:03:25 +0200

    Fix nyxt package file name

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,5 @@
 self: super: {
-  lispPackages = super.lispPackages // (import ./nyxt2.nix {
+  lispPackages = super.lispPackages // (import ./nyxt.nix {
     inherit (super.lispPackages) cl-webkit2 nyxt;
     inherit (super) fetchFromGitHub;
   });
