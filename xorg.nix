5884f6f8d7d4ea75ec6dc3cb645685196473a3d9
Alberto Berti <alberto@metapensiero.it>
2021-12-10 16:10:35 +0100

    Install X with I3 but started only via "startx"

--- xorg.nix
+++ xorg.nix
@@ -1,28 +1,11 @@
 { config, lib, pkgs, ... }: {
   # Enable the X11 windowing system.
   services.xserver = {
-    enable = !config.system.useWayland;
+    enable = true;
     layout = "it";
     xkbOptions = "eurosign:e";
     # Enable touchpad support.
     libinput.enable = true;
-    desktopManager.enlightenment.enable = false;
-    desktopManager.mate.enable = false;
-    desktopManager.pantheon.enable = false;
-    windowManager.i3.enable = true;
-    windowManager.i3.extraSessionCommands = ''
-      nm-applet &
-      EDITOR=emacsclient
-      DPI=210
-      TERMINAL=sakura
-      XCURSOR_SIZE=48
-      export EDITOR TERMINAL XCURSOR_SIZE DPI
-      echo Xft.dpi: $DPI | xrdb -merge
-      xset s off
-      xset dpms 600 600
-      xset +dpms
-
-      ${pkgs.xss-lock}/bin/xss-lock -- i3lock-color -n -B5&
-  '';
+    displayManager.startx.enable = true;
   };
 }
