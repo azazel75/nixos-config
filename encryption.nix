24ef27d334cda0fc8c26266a39d33c36ab66c06b
Alberto Berti <alberto@metapensiero.it>
2021-12-10 16:06:40 +0100

    Update encryption to work on bean, with latest sedutil

--- encryption.nix
+++ encryption.nix
@@ -5,10 +5,7 @@
       patches = (oldAttrs.patches or []) ++ [
         # Add support for enabling unlocking when resuming from sleep
         # See: https://github.com/Drive-Trust-Alliance/sedutil/pull/190
-        (builtins.fetchurl {
-          url = https://patch-diff.githubusercontent.com/raw/Drive-Trust-Alliance/sedutil/pull/190.patch;
-          sha256 = "c0618a319eb0c9a6efe9c72db59338232b235079042ccf77b1d690f64f735a42";
-        })
+	./sedutil-190.patch
       ];
     }));
   };
